9814633a093fe320db97315fc4b5f63ac5ed0c66
Pratik Saha <pratiksaha198@gmail.com>
2022-02-21 14:26:07 +0530

    COUNTER APP, pass custom data, update data in reducer

--- src/app/counter/state/counter.reducer.ts
+++ src/app/counter/state/counter.reducer.ts
@@ -1,6 +1,6 @@
 import { initialState } from './counter.state';
 import { Action, createReducer, on } from "@ngrx/store";
-import { decrement, increment, reset } from './counter.actions';
+import { customIncrement, decrement, increment, reset } from './counter.actions';
 
 const _counterReducer = createReducer(
     initialState,
@@ -20,6 +20,13 @@ const _counterReducer = createReducer(
         return {
             ...state,
             counter: 0
+        }
+    }),
+    on(customIncrement, (state, action) => {
+        console.log(action);
+        return {
+            ...state, 
+            counter: state.counter + action.count,
         }
     }),
 );
